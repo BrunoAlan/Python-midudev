21ce1d20965ac0273e4513d78987af3b5748d697
BrunoAlan <alan.brunoab@gmail.com>
2025-04-20 16:58:57 -0300

    Add initial print statements to 01_basic.py

---
+++ 01_basic.py
@@ -0,0 +1,12 @@
+print("Hello world")
+print('Hello World')
+print('Hello','World','multiple','args')
+print('Hello','World', sep='-')
+
+
+print('Thi is print in', end=' ')
+print('one line')
+
+print(42)
+
+
