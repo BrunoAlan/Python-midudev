2d3a3d9a1e2dbbe76921b75594308fe53d5a365f
BrunoAlan <alan.brunoab@gmail.com>
2025-04-20 17:52:04 -0300

    Add 01_if.py with examples of conditional statements in Python

---
+++ 02_flow_control/01_if.py
@@ -0,0 +1,34 @@
+
+# Conditional statements
+
+print("Simple if statement")
+
+age = 18
+
+if age >= 18:
+  print("You are an adult")
+
+
+print("If-else statement")
+age = 15
+if age>= 18:
+  print("You are an adult")
+else:
+  print("You are a minor")
+
+print("elif statement")
+
+calification = 9
+
+if calification >= 9:
+  print("Excellent")
+elif calification >= 7:
+  print("Good")
+elif calification >= 5:
+  print("Regular")
+else:
+  print("Fail")
+
+
+  
+
