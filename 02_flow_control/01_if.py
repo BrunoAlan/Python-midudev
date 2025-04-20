a7c08a192681ddc38905d2c20b8e8eb9f8467dfe
BrunoAlan <alan.brunoab@gmail.com>
2025-04-20 18:46:50 -0300

    Enhance conditional statements examples and add exercises in 01_if.py

--- 02_flow_control/01_if.py
+++ 02_flow_control/01_if.py
@@ -1,58 +1,185 @@
-
-# Conditional statements
-
+###
+# 01 - Sentencias condicionales (if, elif, else)
+# Permiten ejecutar bloques de código solo si se cumplen ciertas condiciones.
+###
 
 from os import system
 if system("clear") != 0: system("cls")
 
-print("Simple if statement")
+print("\n Sentencia simple condicional")
+# Podemos usar la palabra clave "if" para ejecutar un bloque de código
+# solo si se cumple una condición.
+edad = 18
+if edad >= 18:
+  print("Eres mayor de edad")
+  print("¡Felicidades!")
 
-age = 18
+# Si no se cumple la condición, no se ejecuta el bloque de código
+edad = 15
+if edad >= 18:
+  print("Eres mayor de edad")
+  print("¡Felicidades!")
 
-if age >= 18:
-  print("You are an adult")
-
-
-print("If-else statement")
-age = 15
-if age>= 18:
-  print("You are an adult")
+# Podemos usar el comando "else" para ejecutar un bloque de código
+# si no se cumple la condición anterior del if
+print("\n Sentencia condicional con else")
+edad = 15
+if edad >= 18:
+  print("Eres mayor de edad")
 else:
-  print("You are a minor")
+  print("Eres menor de edad")
 
-print("elif statement")
+print("\n Sentencia condicional con elif")
+nota = 5
 
-calification = 9
-
-if calification >= 9:
-  print("Excellent")
-elif calification >= 7:
-  print("Good")
-elif calification >= 5:
-  print("Regular")
+# Además de usar "if" y "else", podemos usar "elif" para determinar
+# múltiples condiciones, ten en cuenta que sólo se ejecutará el primer bloque
+# de código que cumpla la condición (o la del else, si está presente)
+if nota >= 9:
+  print("¡Sobresaliente!")
+elif nota >= 7:
+  print("Notable!")
+elif nota >= 5:
+  print("¡Aprobado!")
 else:
-  print("Fail")
+  print("¡No está calificado!")
 
+print("\n Condiciones múltiples")
+edad = 16
+tiene_carnet = True
 
-## Javascript opertors in Python
-# && -> and
-# || -> or
-# ! -> not
-print("Logical operators")
-  
-has_license = True 
+# Los operadores lógicos en Python son:
+# and: True si ambos operandos son verdaderos
+# or: True si al menos uno de los operandos es verdadero
+# En JavaScript: 
+# && sería and
+# || sería or
 
-age = 18
-if age >= 18 and has_license:
-  print("You can drive")
+# En el caso que seas mayor de edad y tengas carnet...
+# entonces podrás conducir
+if edad >= 18 and tiene_carnet:
+  print("Puedes conducir 🚗")
 else:
-  print("Call the police")
+  print("POLICIA 🚔!!!1!!!")
 
-# on some rare countries
-
-age = 16
-if age >= 18 or has_license:
-  print("You can drive")
+# En un pueblo de Isla Margarita son más laxos y
+# te dejan conducir si eres mayor de edad O tienes carnet
+if edad >= 18 or tiene_carnet:
+  print("Puedes conducir en la Isla Margarita 🚗")
 else:
-  print("Call the police")
+  print("Paga al policía y te deja conducir!!!")
 
+# También tenemos el operador lógico "not"
+# que nos permite negar una condición
+es_fin_de_semana = False
+# JavaScript -> !
+if not es_fin_de_semana:
+  print("¡midu, venga que hay que streamear!")
+
+# Podemos anidar condicionales, uno dentro del otro
+# para determinar múltiples condiciones aunque
+# siempre intentaremos evitar esto para simplificar
+print("\n Anidar condicionales")
+edad = 20
+tiene_dinero = True
+
+if edad >= 18:
+  if tiene_dinero:
+    print("Puedes ir a la discoteca")
+  else:
+    print("Quédate en casa")
+else:
+  print("No puedes entrar a la disco")
+
+# Más fácil sería:
+# if edad < 18:
+#   print("No puedes entrar a la disco")
+# elif tiene_dinero:
+#   print("Puedes ir a la discoteca")
+# else:
+#   print("Quédate en casa")
+
+# Ten en cuenta que hay valores que al usarlos como condiciones
+# en Python son evaluados como verdaderos o falsos
+# por ejemplo, el número 5, es True
+numero = 5
+if numero: # True
+  print("El número no es cero")
+
+# Pero el número 0 se evalúa como False
+numero = 0
+if numero: # False
+  print("Aquí no entrará nunca")
+
+# También el valor vacío "" se evalúa como False
+nombre = ""
+if nombre:
+  print("El nombre no es vacío")
+
+# ¡Ten cuidado con no confundir la asignación = con la comparación ==!
+numero = 3 # asignación
+es_el_tres = numero == 3 # comparación
+
+if es_el_tres:
+  print("El número es 3")
+
+# A veces podemos crear condicionales en una sola línea usando
+# las ternarias, es una forma concisa de un if-else en una línea de código
+print("\nLa condición ternaria:")
+# [código si cumple la condición] if [condicion] else [codigo si no cumple]
+
+edad = 17
+mensaje = "Es mayor de edad" if edad >= 18 else "Es menor de edad"
+print(mensaje)
+
+###
+# EJERCICIOS
+###
+
+# Ejercicio 1: Determinar el mayor de dos números
+# Pide al usuario que introduzca dos números y muestra un mensaje
+# indicando cuál es mayor o si son iguales
+
+number1 = int(input("Introduce el primer número: "))
+number2 = int(input("Introduce el segundo número: "))
+
+if number1 > number2:
+  print(f"El número mayor es {number1}")
+elif number2 > number1:
+  print(f"El número mayor es {number2}")
+else:
+  print("Los números son iguales")
+
+# Ejercicio 2: Calculadora simple
+# Pide al usuario dos números y una operación (+, -, *, /)
+# Realiza la operación y muestra el resultado (maneja la división entre zero)
+
+number1, number2, operator = input("Introduce dos numero y una operación").split()
+
+int_number1 = int(number1)
+int_number2 = int(number2)
+
+
+if int_number2 == 0 and operator == '/':
+  print('No se puede dividir por cero')
+elif operator == '+':
+  print(int_number1+int_number2)
+elif operator == '-':
+  print(int_number1-int_number2)
+elif operator == '*':
+  print(int_number1*int_number2)
+elif operator == '/':
+  print(int_number1/int_number2)  
+
+
+# Ejercicio 3: Año bisiesto
+# Pide al usuario que introduzca un año y determina si es bisiesto.
+# Un año es bisiesto si es divisible por 4, excepto si es divisible por 100 pero no por 400.
+
+# Ejercicio 4: Categorizar edades
+# Pide al usuario que introduzca una edad y la clasifique en:
+# - Bebé (0-2 años)
+# - Niño (3-12 años)
+# - Adolescente (13-17 años)
+# - Adulto (18-64 años)
+# - Adulto mayor (65 años o más)
