f04c108494dabd34b769054093e2fa7f9c161e89
BrunoAlan <alan.brunoab@gmail.com>
2025-04-20 16:59:09 -0300

    Add 03_casting.py with examples of data type conversion in Python

---
+++ 03_casting.py
@@ -0,0 +1,18 @@
+
+# Casting - Converting one data type to another
+
+print("Conversion de tipo")
+
+print(type("100")) # str
+print(type(int("100"))) # int
+
+print(bool("")) # False
+print(bool(" ")) # True
+print(bool(0)) # False
+print(bool(1)) # True
+print(bool([])) # False
+print(bool([1, 2, 3])) # True
+
+print(int(2.5)) # 2
+print(int(2.9)) # 2
+
