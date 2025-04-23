f39104ba6b742d93283d724a01719389845f4655
BrunoAlan <alan.brunoab@gmail.com>
2025-04-23 12:17:11 -0300

    Refactor find_first_sum function for efficiency and readability; add new battle simulation functionality with detailed explanation

--- 04_logic/03_find_first_sum.py
+++ 04_logic/03_find_first_sum.py
@@ -7,12 +7,21 @@ goal = 8
 find_first_sum(nums, goal)  # [2, 3]
 """
 
+# def find_first_sum(nums: list, goal: int) -> list:
+#     for i in range(len(nums) - 1):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] + nums[j] == goal:
+#                 return [i, j]
+
 
 def find_first_sum(nums: list, goal: int) -> list:
-    for i in range(len(nums) - 1):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == goal:
-                return [i, j]
+    seen = {}  # diccionario para guardar el nro y su indice
+    for index, value in enumerate(nums):
+        missing = goal - value
+        if missing in seen:
+            return [seen[missing], index]
+        seen[value] = index
+    return None
 
 
-find_first_sum([1, 2, 3, 4, 5], 4)
+print(find_first_sum([1, 2, 3, 4, 5], 4))
