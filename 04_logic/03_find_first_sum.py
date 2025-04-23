3a5ba571407d8ebf44d90ec2d7b4b64117c8f69c
BrunoAlan <alan.brunoab@gmail.com>
2025-04-23 11:20:13 -0300

    Refactor find_first_sum function for improved readability and consistency

--- 04_logic/03_find_first_sum.py
+++ 04_logic/03_find_first_sum.py
@@ -7,13 +7,12 @@ goal = 8
 find_first_sum(nums, goal)  # [2, 3]
 """
 
-def find_first_sum(nums: list, goal:int) ->list:
-  for  i in range(len(nums)-1):
-    for j in range(i+1,len(nums)):
-      if nums[i]+nums[j] == goal:
-        return [i,j]
+
+def find_first_sum(nums: list, goal: int) -> list:
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == goal:
+                return [i, j]
 
 
-find_first_sum([1,2,3,4,5],4)
-
-
+find_first_sum([1, 2, 3, 4, 5], 4)
